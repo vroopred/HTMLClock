df608be18eedfbff8603d40a6c1a9c24bda56f0d
Varsha Roopreddy <vroopred@calpoly.edu>
2015-01-22 23:25:33 -0800

    Shorter Javascript Code
    Used toLocaleTimeString() to get the Time string from the Date object.

--- js/clock.js
+++ js/clock.js
@@ -1,12 +1,5 @@
 function getTime() {
-    var date=new Date();
-    var hours=date.getHours();
-    var minutes=date.getMinutes();
-    var seconds=date.getSeconds();
-    if(minutes<10) 
-    	{minutes = "0"+minutes};
-    if(seconds<10) 
-    	{seconds = "0"+seconds}; 
-    document.getElementById('clock').innerHTML = hours+":"+minutes+":"+seconds;
+    var time = new Date().toLocaleTimeString();
+    document.getElementById('clock').innerHTML = time;
     setTimeout(function(){getTime()},1000);
 }
