c8ce049ce88cfb520a3f8a10883707cb4859d05e
Varsha Roopreddy <vroopred@calpoly.edu>
2015-01-29 20:12:11 -0800

    Lab 4 final

--- js/clock.js
+++ js/clock.js
@@ -1,5 +1,39 @@
+
 function getTime() {
     var time = new Date().toLocaleTimeString();
     document.getElementById('clock').innerHTML = time;
-    setTimeout(function(){getTime()},1000);
+     setTimeout(function(){getTime()},1000);
+
+}
+
+function getTemp() {
+	$.getJSON("https://api.forecast.io/forecast/c6debd55d1caf6b070daba6dd3e2cbeb/35.300399,-120.662362?callback=?", 
+		function(forecast) {
+			$("#forecastLabel").html(forecast.daily.summary);
+			var image = "img/" + forecast.daily.icon + ".png";
+			$("#forecastIcon").attr("src", image);
+
+			var temp = forecast.daily.data[0].temperatureMax;
+			if(temp < 60) {
+				$("body").addClass("cold");
+			}
+			else if(temp >= 60) {
+				$("body").addClass("chilly");
+			}
+			else if(temp >= 70) {
+				$("body").addClass("nice");
+			}
+			else if(temp >= 80) {
+				$("body").addClass("warm");
+			}
+			else {
+				$("body").addClass("hot");
+			}
+		});
+}
+
+
+function getInfo() {
+	getTemp();
+	getTime();
 }
