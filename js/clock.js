3bcfd829012ca0d5ef34e643eb1fb52b91dd4b74
vroopred <classroomtech@is-ct-a015627.local>
2015-02-04 21:44:33 -0800

    Partly working lab 5

--- js/clock.js
+++ js/clock.js
@@ -32,8 +32,68 @@ function getTemp() {
 		});
 }
 
-
 function getInfo() {
 	getTemp();
 	getTime();
 }
+
+function showAlarmPopup() {
+	$("#mask").removeClass("hide");
+	$("#popup").removeClass("hide");
+}
+
+function hideAlarmPopup() {
+	$("#mask").addClass("hide");
+	$("#popup").addClass("hide");
+}
+
+function insertAlarm(hours, mins, ampm, alarmName) {
+	var blankDiv = $("<div></div>").addClass("flexable");
+	var div1 = $("<div></div>").addClass("name");
+	div1.html(alarmName);
+	var div2 = $("<div></div>").addClass("time");
+	div2.html(hours + ":" + mins +ampm);
+	blankDiv.append(div1);
+	blankDiv.append(div2);
+	$("#alarms").append(blankDiv);
+}
+
+
+function addAlarm() {
+	var hours = $("#hours option:selected").text();
+	var mins = $("#mins option:selected").text();
+	var ampm = $("#ampm option:selected").text();
+	var alarmName = $("#alarmName").val();
+
+	var AlarmObject = Parse.Object.extend("Alarm");
+    var alarmObject = new AlarmObject();
+    hideAlarmPopup();
+      alarmObject.save({"hours": hours, "mins": mins, "ampm": ampm, "alarmName": alarmName}, {
+      success: function(object) {
+        insertAlarm(hours, mins, ampm, alarmName);
+		hideAlarmPopup();
+      }
+    });
+}
+
+function getAllAlarms() {
+	Parse.initialize("vC5Npzg5L5xeSOOdLClryl4cfpC0cuPHuTMoKpXH", "1jC5M6BzOI3r352eoaa8UbUMYbCkJWswWLCEhvvF");
+	    var AlarmObject = Parse.Object.extend("Alarm");
+    var query = new Parse.Query(AlarmObject);
+    query.find({
+        success: function(results) {
+          for (var i = 0; i < results.length; i++) { 
+            insertAlarm(results[i].get("hours"), results[i].get("mins"), results[i].get("ampm"), results[i].get("alarmName"));
+          }
+        }
+    });
+}
+
+
+
+
+
+
+
+
+
